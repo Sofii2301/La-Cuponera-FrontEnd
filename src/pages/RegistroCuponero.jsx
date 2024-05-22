73acb5fbada77726508b92f51b8c3dc9bea8d76f
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-05-22 14:19:22 -0300

    Registro cuponero

--- src/pages/RegistroCuponero.jsx
+++ src/pages/RegistroCuponero.jsx
@@ -9,16 +9,15 @@ export default function RegistroCuponero(props) {
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
         //__id: 0,
-        firstName: "",
-        lastName: "",
+        nombre: "",
+        apellido: "",
         email: "",
-        password: "",
+        contraseña: "",
         //__v: 0
         //registroFecha: new Date().toISOString(), // Fecha actual
         //estadoVerificacion: "pendiente", // Estado inicial
     });
     const [formErrors, setFormErrors] = useState({
-        id: "",
         firstName: '',
         lastName: '',
         email: '',
@@ -51,9 +50,9 @@ export default function RegistroCuponero(props) {
             localStorage.setItem("cuponeroData", JSON.stringify({ id: cuponeroId, ...formData, registroCuponero: true}));
             
             const cuponeroData = JSON.parse(localStorage.getItem("cuponeroData"));
-            console.log("RegistroCuponero: ", cuponeroData.registroCuponero);
+            console.log("RegistroCuponero: ", cuponeroData.registroCuponero);*/
 
-            const userType = 'cuponero'; // o 'cuponero', dependiendo del tipo de registro*/
+            const userType = 'cuponero'; // o 'cuponero', dependiendo del tipo de registro
             navigate(`/signup/verify/${userType}/${formData.email}`);
         } catch (err) {
             console.error('Error:', err);
@@ -66,12 +65,12 @@ export default function RegistroCuponero(props) {
         const errors = {};
 
         // Validar cada campo
-        if (formData.firstName.trim() === '') {
+        if (formData.nombre.trim() === '') {
             setErrorMessage('Por favor, ingresa tu nombre');
             isValid = false;
         }
 
-        if (formData.lastName.trim() === '') {
+        if (formData.apellido.trim() === '') {
             setErrorMessage('Por favor, ingresa tu apellido');
             isValid = false;
         }
@@ -81,7 +80,7 @@ export default function RegistroCuponero(props) {
             isValid = false;
         }
 
-        if (formData.password.trim() === '') {
+        if (formData.contraseña.trim() === '') {
             setErrorMessage('Por favor, ingresa tu contraseña');
             isValid = false;
         }
@@ -98,12 +97,12 @@ export default function RegistroCuponero(props) {
                 <div className="mb-3 fila-rc">
                     <div className="col-rc">
                         <label htmlFor="formSignupfname" className="form-label visually-hidden">Nombre</label>
-                        <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className={`form-control ${formErrors.firstName && 'is-invalid'}`} id="formSignupfname" placeholder="Nombre" required />
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} className={`form-control ${formErrors.firstName && 'is-invalid'}`} id="formSignupfname" placeholder="Nombre" required />
                         <div className="invalid-feedback">{formErrors.firstName}</div>
                     </div>
                     <div className="col-rc">
                         <label htmlFor="formSignuplname" className="form-label visually-hidden">Apellido</label>
-                        <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className={`form-control ${formErrors.lastName && 'is-invalid'}`} id="formSignuplname" placeholder="Apellido" required />
+                        <input type="text" name="apellido" value={formData.apellido} onChange={handleChange} className={`form-control ${formErrors.lastName && 'is-invalid'}`} id="formSignuplname" placeholder="Apellido" required />
                         <div className="invalid-feedback">{formErrors.lastName}</div>
                     </div>
                 </div>
@@ -114,7 +113,7 @@ export default function RegistroCuponero(props) {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formSignupPassword" className="form-label visually-hidden">Contraseña</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} className={`form-control ${formErrors.password && 'is-invalid'}`} id="formSignupPassword" placeholder="Contraseña" required />
+                    <input type="password" name="contraseña" value={formData.contraseña} onChange={handleChange} className={`form-control ${formErrors.password && 'is-invalid'}`} id="formSignupPassword" placeholder="Contraseña" required />
                     {/* colocar un ojo para ver la contraseña */}
                     <div className="invalid-feedback">{formErrors.password}</div>
                 </div>
