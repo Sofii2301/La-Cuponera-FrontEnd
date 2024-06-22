48ab14b101a674b5edfbf6fd557faf912804889e
marcexl <mxlgallardo@gmail.com>
2024-06-22 17:29:20 -0300

    [main] update config apibase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // src/config.js
-export const API_BASE_URL_VENDEDOR = 'https://la-cuponera-vendedores-2-a1e6fa4de317.herokuapp.com/api/vendedores';
+export const API_BASE_URL_VENDEDOR = 'https://la-cuponera-vendedores-mysql-f4e866001235.herokuapp.com/api/vendedores';
 export const API_BASE_URL_CUPONERO = 'https://la-cuponera-cuponeros-mysql-fd1ec2c2535c.herokuapp.com/api/cuponeros';
 export const API_BASE_URL_CUPONES = 'https://la-cuponera-cupones-mysql-30ef44aef9d6.herokuapp.com/api/cupones';
 export const API_BASE_URL_IMAGEN = 'https://lacuponera-vendedores.vercel.app/api/upload';
