e91c1126c01d7a65fc8593c31574a43c4043d285
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-06-27 12:30:29 -0300

    arreglo bienvenida home vendedor

--- src/pages/Vendedor/Home_V.jsx
+++ src/pages/Vendedor/Home_V.jsx
@@ -13,17 +13,16 @@ export default function Home_V() {
         <>
                 
             <Vendedor> 
-                <h1 className=" tituloCont titulo">Bienvenidos a</h1>
-                    <div className="container-cd homeContenedor w-100 d-flex align-items-center justify-content-center" >
-                        <div className="winwinCont">
-                            <img src={Winwin} alt="" />
+                <div className="container-cd">
+                    <div className="row countdown-container-slideIn">
+                        <div className="col-xxl-7 col-xl-7 col-lg-6 col-md-6 col-12 div-cd-text d-flex flex-column align-items-center">
+                            <h1 className="titulo tituloCont pb-2">Bienvenido de nuevo a</h1>
+                            <img className="logoTitulo img-fluid" src={Logo} alt=""/>
                         </div>
-                        <div>
-                            <img className="logoTitulo" src={Logo} alt=""/>
+                        <div className="col-xxl-5 col-xl-5 col-lg-6 col-md-6 col-12 div-cuponik-cd">
+                            <img className="cuponik-cd-hv" src={Cuponik} alt="" />
                         </div>
-                        <div className="cuponikCont">
-                            <img src={Cuponik} alt="" />
-                        </div>
+                    </div>
                     </div>
                 <MapStores/>
             </Vendedor>
