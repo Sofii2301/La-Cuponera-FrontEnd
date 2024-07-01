831766e08f24e2c31c46fe1b7e77fe30b41278de
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-07-01 17:26:06 -0300

    banners ganadores

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Carousel } from 'antd';
+import { useMediaQuery } from '@mui/material';
 
 import Banner1 from '../assets/lacuponera.jpg';
 import Banner2 from '../assets/PortadaMail(2).jpg';
+import MaysoonMakeUp1 from '../assets/banners/banners_ganadores-04-01.jpg';
+import MaysoonMakeUp2 from '../assets/banners/banners_ganadores-04-02.jpg';
+import MaysoonMakeUp3 from '../assets/banners/banners_ganadores-04-03.jpg';
 
 const contentStyle = {
     height: '300px',
@@ -13,17 +17,46 @@ const contentStyle = {
     width: '100%',
 };
 
-const banners = [
-    Banner1,
+const bannerGeneral = [
+    Banner1
 ];
 
-const Carrousel = () => (
-    <Carousel autoplay>
-        {banners.map((image, index) => (
-            <div key={index}>
-                <img src={image} alt={`slide-${index}`} style={{ ...contentStyle, objectFit: 'cover', width: '100%' }} />
-            </div>
-        ))}
-    </Carousel>
-);
+const bannersXl = [
+    MaysoonMakeUp1,
+];
+
+const bannersMd = [
+    MaysoonMakeUp2,
+];
+
+const bannersSm = [
+    MaysoonMakeUp3
+];
+
+const Carrousel = () => { 
+    const esPantallaGrande = useMediaQuery('(min-width: 1200px)');
+    const esPantallaMediana = useMediaQuery('(min-width: 768px)');
+
+    const [banners, setBanners] = useState([]);
+
+    useEffect(() => {
+        if (esPantallaGrande) {
+            setBanners(bannersXl);
+        } else if(esPantallaMediana) {
+            setBanners(bannersMd);
+        } else {
+            setBanners(bannersSm);
+        }
+        setBanners(bannerGeneral);
+    }, []);
+    return ( 
+        <Carousel autoplay>
+            {banners.map((image, index) => (
+                <div key={index}>
+                    <img src={image} alt={`slide-${index}`} style={{ ...contentStyle, objectFit: 'cover', width: '100%' }} />
+                </div>
+            ))}
+        </Carousel>
+    )
+};
 export default Carrousel;
