0c67428da5c822b6e139d3bb0fbe354aa4b3421a
Niico182 <suareznicolas182@gmail.com>
2024-07-22 09:40:19 -0300

    cambios en account

---
+++ src/pages/Cuponero/AccountEdit.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "../../css/Cuponero/account.css"
+import NavCuponeros from "../../components/Cuponero/NavCuponeros"
+
+export default function AccountEdit() {
+  return (
+    <div>
+    <NavCuponeros></NavCuponeros>
+    
+    </div>
+  )
+}
