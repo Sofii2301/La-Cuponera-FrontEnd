2405901b14d5cf989960d6835e7e5477ee543da6
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-07-23 00:54:16 -0300

    Fix carrusel

--- src/pages/Cuponero/CercaAVos.jsx
+++ src/pages/Cuponero/CercaAVos.jsx
@@ -5,7 +5,7 @@ import Carrousel from "../../components/Carrousel"
 import MapStores from '../../components/MapStores'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import {  responsive } from "../../js/slider";
+import {  responsiveCV } from "../../js/slider";
 import Cupon from "../../components/Cupones/Cupon";
 import Vendedor from "../../components/Cuponero/Vendedor"
 import { getCouponImage, getCoupons } from "../../services/CuponesService";
@@ -41,26 +41,28 @@ export default function CercaAVos() {
     }, []);
 
     const cupon = cupones.map((item, index) => (
-        <Cupon
-            key={index}
-            discount={item.discount}
-            id={item.id} 
-            categorias={item.categorias}
-            title={item.title}
-            raiting={item.raiting}
-            price={item.price}
-        />
+        <div key={index} className="carousel-item-wrapper">
+            <Cupon
+                discount={item.discount}
+                id={item.id}
+                categorias={item.categorias}
+                title={item.title}
+                raiting={item.raiting}
+                price={item.price}
+            />
+        </div>
     ));
 
     const tienda = vendedores.map((item, index) => (
-        <Vendedor
-            key={index}
-            id={item.vendedor_id} 
-            vendedor_id={item.vendedor_id} 
-            nombreTienda={item.nombreTienda}
-            categorias={item.categorias}
-            raiting={item.raiting}
-        />
+        <div key={index} className="carousel-item-wrapper">
+            <Vendedor
+                id={item.vendedor_id}
+                vendedor_id={item.vendedor_id}
+                nombreTienda={item.nombreTienda}
+                categorias={item.categorias}
+                raiting={item.raiting}
+            />
+        </div>
     ));
 
     return(
@@ -74,13 +76,13 @@ export default function CercaAVos() {
                 </div>
                 <div className="mt-5 ml-3 pt-0 pb-5 carousel-cupones">
                     <h3>Cupones destacados: </h3>
-                    <Carousel className="carousel-cupones" itemClass="carousel-item-custom" showDots={true} responsive={responsive}>
+                    <Carousel className="carousel-cupones" itemClass="carousel-item-custom" showDots={true} responsive={responsiveCV}>
                     {cupon}
                     </Carousel>
                 </div>
                 <div className="mt-3 ml-3 pt-5 pb-5 carousel-vendedores">
                     <h3>Tiendas certificadas: </h3>
-                    <Carousel className="carousel-vendedores" itemClass="carousel-item-custom" showDots={true} responsive={responsive}>
+                    <Carousel className="carousel-vendedores" itemClass="carousel-item-custom" showDots={true} responsive={responsiveCV}>
                     {tienda}
                     </Carousel>
                 </div>
