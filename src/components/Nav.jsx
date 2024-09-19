e13310560d2fbc93963569c5ae7da279e0cde4de
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-09-19 20:26:53 -0300

    fix navbar y sidebar vendedores

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,66 +1,72 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import AppBar from '@mui/material/AppBar';
 
 export default function Nav({ isSignIn, children, children2, link }) {
+    const [position, setPosition] = useState(0);
+
+    window.addEventListener('scroll', function() {
+        const scrollY = this.scrollY;
+        setPosition(scrollY);
+    });
+
     return(
         <>
-            <div className="border-bottom shadow-sm">
-                <div className="navbar navbar-light py-2">
-                    <div className="container-navbar">
-                        <div className="row row-nav">
-                            {children && (
-                                <div className="col-1">
-                                    {children}
-                                </div>
-                            )}
-                            <div className="col-xxl-2 col-lg-3 col-md-5 col-10">
-                                <Link to={link ? link : '/'} className="navbar-brand-logo">
-                                    <img src={logo} alt="" className="d-inline-block align-text-top logo-navbar" />
-                                </Link>
+            <AppBar className="border-bottom shadow-sm navbar navbar-light py-2" position={position <= 0 ? 'static' : 'fixed'}>
+                <div className="container-navbar">
+                    <div className="row row-nav">
+                        {children && (
+                            <div className="col-1">
+                                {children}
                             </div>
-                            {isSignIn && (
-                                <div className="col-xxl-5 col-lg-5 col-md-6 col-12">
-                                    <span className="navbar-text">
-                                        {isSignIn === "sesion-c" && (
-                                            <>
-                                                ¿Ya tenés una cuenta? <Link to="/" style={{textDecoration: "none"}}>Iniciar sesión</Link>
-                                            </>
-                                        )}
-                                        {isSignIn === "sesion-v" && (
-                                            <>
-                                                ¿Ya tenés una cuenta? <Link to="/signin/vendedor" style={{textDecoration: "none"}}>Iniciar sesión</Link>
-                                            </>
-                                        )}
-                                        {isSignIn === "registro" && (
-                                            <>
-                                                ¿Aún no tenés tu cuenta? <Link to="/" style={{textDecoration: "none"}}>Registrate</Link>
-                                            </>
-                                        )}
-                                        {isSignIn === "registro-v" && (
-                                            <>
-                                                ¿Aún no tenés tu cuenta? <Link to="/signup/vendedor" style={{textDecoration: "none"}}>Registrate</Link>
-                                            </>
-                                        )}
-                                        {isSignIn === "registro-tienda" && (
-                                            <>
-                                                ¿Tenés tu propio negocio? <Link to="/signup/vendedor" style={{textDecoration: "none"}}>Ser Vendedor</Link>
-                                            </>
-                                        )}
-                                        {isSignIn !== "registro" && isSignIn !== "sesion" && ""}
-                                    </span>
-                                </div>
-                            )}
-                            {children2 && (
-                                <div className="col-1">
-                                    {children2}
-                                </div>
-                            )}
+                        )}
+                        <div className="col-xxl-2 col-lg-3 col-md-5 col-10">
+                            <Link to={link ? link : '/'} className="navbar-brand-logo">
+                                <img src={logo} alt="" className="d-inline-block align-text-top logo-navbar" />
+                            </Link>
                         </div>
-                    </div>                   
+                        {isSignIn && (
+                            <div className="col-xxl-5 col-lg-5 col-md-6 col-12">
+                                <span className="navbar-text">
+                                    {isSignIn === "sesion-c" && (
+                                        <>
+                                            ¿Ya tenés una cuenta? <Link to="/" style={{textDecoration: "none"}}>Iniciar sesión</Link>
+                                        </>
+                                    )}
+                                    {isSignIn === "sesion-v" && (
+                                        <>
+                                            ¿Ya tenés una cuenta? <Link to="/signin/vendedor" style={{textDecoration: "none"}}>Iniciar sesión</Link>
+                                        </>
+                                    )}
+                                    {isSignIn === "registro" && (
+                                        <>
+                                            ¿Aún no tenés tu cuenta? <Link to="/" style={{textDecoration: "none"}}>Registrate</Link>
+                                        </>
+                                    )}
+                                    {isSignIn === "registro-v" && (
+                                        <>
+                                            ¿Aún no tenés tu cuenta? <Link to="/signup/vendedor" style={{textDecoration: "none"}}>Registrate</Link>
+                                        </>
+                                    )}
+                                    {isSignIn === "registro-tienda" && (
+                                        <>
+                                            ¿Tenés tu propio negocio? <Link to="/signup/vendedor" style={{textDecoration: "none"}}>Ser Vendedor</Link>
+                                        </>
+                                    )}
+                                    {isSignIn !== "registro" && isSignIn !== "sesion" && ""}
+                                </span>
+                            </div>
+                        )}
+                        {children2 && (
+                            <div className="col-1">
+                                {children2}
+                            </div>
+                        )}
+                    </div>
                 </div>
-            </div>
+            </AppBar>
         </>
     );
 }
